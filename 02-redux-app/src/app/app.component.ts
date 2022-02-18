0941b5a878684e8535be8e20c5c51b371533253c
Anartz Mugika Ledo <mugan86@gmail.com>
2022-02-18 19:56:35 +0100

    feature: Class 30-Listen specify changes 'store.select()'

--- 02-redux-app/src/app/app.component.ts
+++ 02-redux-app/src/app/app.component.ts
@@ -14,13 +14,18 @@ export class AppComponent {
   title = 'redux-app';
   counter: number;
 
-  // count$: Observable<number>
+  count$: Observable<number>
 
   constructor (private store: Store<IAppState>) {
     this.counter = 10;
+    // Use to take all select state
     this.store.subscribe((state) => {
       this.counter = state.count;
     });
+    // Take only one select element with observable
+    this.count$ = this.store.select('count');
+    // whitout observable
+    this.store.select('count').subscribe(( counter ) => console.log(counter));
   }
 
   increment = () => this.store.dispatch(increment())
