db367409f7e25e534a4a3ddb8569ed20154fa839
Anartz Mugika Ledo <mugan86@gmail.com>
2022-02-18 17:54:25 +0100

    feature: Class 16. Finish class

--- 01-redux-basic/app.1.ts
+++ 01-redux-basic/app.1.ts
@@ -1,32 +1,51 @@
 // Action: Specify that action
 
-interface IAction {
+interface Action {
   type: string;
   payload?: any; // Params to add in specify action
 }
 
-const reducer = (oldState = 10, action: IAction) => {
-  if (action.type === "INCREMENT") {
-    return (oldState += 1);
+const reducer = (oldState = 10, action: Action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return (oldState + 1);
+    case "DECREMENT":
+      return (oldState - 1);
+    case "MULTIPLY":
+        return (oldState * action.payload);
+    case "DIVISION":
+        return (oldState / action.payload);
+    default:
+      return oldState;
   }
-  if (action.type === "DECREMENT") {
-    return (oldState -= 1);
-  }
-  return oldState;
 };
 
 // Actions definitions
 
-const incrementAction: IAction = {
+const incrementAction: Action = {
   type: "INCREMENT",
 };
 
-const decrementAction: IAction = {
+const decrementAction: Action = {
   type: "DECREMENT",
 };
 
-console.log(reducer(10, incrementAction));
+const multiplyAction: Action = {
+  type: "MULTIPLY",
+  payload: 2
+};
 
-console.log(reducer(134, incrementAction));
+const divisionAction: Action = {
+  type: "DIVISION",
+  payload: 2
+};
 
-console.log(reducer(10, decrementAction));
+console.log(reducer(10, incrementAction)); // 11
+
+console.log(reducer(134, incrementAction)); // 135
+
+console.log(reducer(10, decrementAction)); // 9
+
+console.log(reducer(10, multiplyAction)); // 20
+
+console.log(reducer(10, divisionAction)); // 5
