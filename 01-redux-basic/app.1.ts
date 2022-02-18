749fa7b81c81fe1b6239db886f3da49718cd02ab
Anartz Mugika Ledo <mugan86@gmail.com>
2022-02-18 11:02:14 +0100

    feature: Class 15. Actions and Reducer implement and testing

--- 01-redux-basic/app.1.ts
+++ 01-redux-basic/app.1.ts
@@ -1,1 +1,32 @@
-console.log("Hello world");
+// Action: Specify that action
+
+interface IAction {
+  type: string;
+  payload?: any; // Params to add in specify action
+}
+
+const reducer = (oldState = 10, action: IAction) => {
+  if (action.type === "INCREMENT") {
+    return (oldState += 1);
+  }
+  if (action.type === "DECREMENT") {
+    return (oldState -= 1);
+  }
+  return oldState;
+};
+
+// Actions definitions
+
+const incrementAction: IAction = {
+  type: "INCREMENT",
+};
+
+const decrementAction: IAction = {
+  type: "DECREMENT",
+};
+
+console.log(reducer(10, incrementAction));
+
+console.log(reducer(134, incrementAction));
+
+console.log(reducer(10, decrementAction));
