18f5b3102b11ee8f230ec81a5c8f7a6c076e20db
Anartz Mugika Ledo <mugan86@gmail.com>
2022-02-19 19:31:18 +0100

    feature: Class 40-Add app all components

--- 03-todo-app/src/app/app.module.ts
+++ 03-todo-app/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { TodoModule } from './todos/todo.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 03-todo-app/src/app/todos/todo.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoAppComponent } from './todo-app/todo-app.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
+
+@NgModule({
+  declarations: [
+    TodoAppComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoPageComponent,
+  ],
+  imports: [CommonModule],
+})
+export class TodoModule {}
